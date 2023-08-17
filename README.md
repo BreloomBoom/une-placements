26aa49539e6d753deb99abe20dbc8c597afebca6
Ronald Chiang <z5419773@ad.unsw.edu.au>
2023-08-18 08:50:21 +1000

    adding assignments to year 4 year 5 and pathways

--- README.md
+++ README.md
@@ -2,22 +2,34 @@
 
 A simple script which assigns clinical schools to students based on their preferences and availabilities.
 
-# Adding Schools
+## Adding Schools
 
-Add a text file in csv format where the ____ column is ____
-- First: School Name
-- Second: Capacity for the School
+Add a text file in csv format where the column ____ is ____
+- 1: School Name
+- 2: Capacity for the School
+- 3: 'M' if Metro and 'R' if Rural
 
-# Adding Students
-Add a text file in csv format where the ____ column is ____
+## Adding Pathways
 
-- First: Student Name
-- Second: Ranking for the First School
-- Third: Ranking for the Second School
-- Fourth: Ranking for the Third School
-and so on...
+Add a text file in csv format where the column ____ is ____
+- 1: Pathway Name
+- 2: Capacity for the Pathway
 
-The order of schools will match the order in the school file.
+## Adding Students
+
+Assuming there are n schools and m pathways
+
+Add a text file in csv format where the column ____ is ____
+
+- 1: Student Name
+- 2 to n + 1: Ranks for Year 4 Schools
+- n + 2 to 2n + 1: Ranks for Year 5 Schools
+- 2n + 2 to 2n + m + 1: Ranks for Pathways
+- 2n + m + 2 to 2n + m + 4: Rank for Year 4, Year 5, Pathway
+
+The order of schools will match the order in the school file
+
+The order of pathways will match the order of the pathway file
 
 # Using the program
 
@@ -31,4 +43,9 @@ The order of schools will match the order in the school file.
 
 e.g.
 
-```python3 placements.py schools_ex.csv students_ex.csv```
+```python3 placements.py schools_ex.csv pathways_ex.csv students_ex.csv```
+
+## Current Constraints
+- Every student gets assigned 1 school in Year 4, Year 5 and 1 Pathway
+- At least 1 school must be metro
+- A higher preference for pathways scales you down year 4 and year 5 etc.
--- placements.py
+++ placements.py
@@ -2,53 +2,91 @@ import csv
 import sys
 from ortools.linear_solver import pywraplp
 
-if len(sys.argv) != 3:
-    print('Usage: python3 placements.py [school file] [student file]')
+# Scaling for preference between Year 4, Year 5 and Pathways
+scaling = [1, 1.2, 1.5]
+
+if len(sys.argv) != 4:
+    print('Usage: python3 placements.py [school file] [pathway file] [student file]')
     sys.exit(1)
 
 schools = list(csv.reader(open(sys.argv[1], 'r'), delimiter=','))
-students = list(csv.reader(open(sys.argv[2], 'r'), delimiter=','))
-ranks = [[int(rank) for rank in student[1:]] for student in students]
+pathways = list(csv.reader(open(sys.argv[2], 'r'), delimiter=','))
+students = list(csv.reader(open(sys.argv[3], 'r'), delimiter=','))
 
 num_students = len(students)
+num_pathways = len(pathways)
 num_schools = len(schools)
+
+y4_ranks = [[int(rank) * scaling[int(student[2 * num_schools + num_pathways + 1]) - 1] for rank in student[1:num_schools + 1]] for student in students]
+y5_ranks = [[int(rank) * scaling[int(student[2 * num_schools + num_pathways + 2]) - 1] for rank in student[num_schools + 1:2 * num_schools + 1]] for student in students]
+path_ranks = [[int(rank) * scaling[int(student[2 * num_schools + num_pathways + 3]) - 1] for rank in student[2 * num_schools + 1:2 * num_schools + num_pathways + 1]] for student in students]
 
 if num_students > sum([int(capacity[1]) for capacity in schools]):
     print('There are more students than school spots available')
     sys.exit(1)
 
-if any(len(student) <= num_schools for student in students):
-    print('Students needs to have a ranking for all schools')
+if num_students > sum([int(capacity[1]) for capacity in pathways]):
+    print('There are more students than pathway spots available')
+    sys.exit(1)
+
+if any(len(student) <= 2 * num_schools + num_pathways for student in students):
+    print('Students needs to have a ranking for all schools and pathways')
     sys.exit(1)
 
 solver = pywraplp.Solver.CreateSolver("SCIP")
 
-# x[i, j] is an array of 0-1 variables, which will be 1
-# If student i is assigned to school j
-x = {}
+# Having the solver assign either 0 or 1 where 1 means  the option is chosen
+y4 = {}
+y5 = {}
+path = {}
 for i in range(num_students):
     for j in range(num_schools):
-        x[i, j] = solver.IntVar(0, 1, "")
+        y4[i, j] = solver.IntVar(0, 1, "")
+        y5[i, j] = solver.IntVar(0, 1, "")
+    
+    for j in range(num_pathways):
+        path[i, j] = solver.IntVar(0, 1, "")
 
-# Each student is assigned to exactly 1 school
+# Constraints
+
+# Each student is assigned to exactly 1 school in y4, y5 and 1 pathway
 for i in range(num_students):
-    solver.Add(solver.Sum([x[i, j] for j in range(num_schools)]) == 1)
+    solver.Add(solver.Sum([y4[i, j] for j in range(num_schools)]) == 1)
+    solver.Add(solver.Sum([y5[i, j] for j in range(num_schools)]) == 1)
+    solver.Add(solver.Sum([path[i, j] for j in range(num_pathways)]) == 1)
+    # There must be at least 1 metro school between y4 and y5
+    solver.Add(solver.Sum([y4[i, j] * int(schools[j][2] == 'M') for j in range(num_schools)]) + 
+               solver.Sum([y5[i, j] * int(schools[j][2] == 'M') for j in range(num_schools)]) >= 1)
 
-# Each school is assigned to at most the given capacity
+# Each school and pathway is assigned to at most the given capacity
 for j in range(num_schools):
-    solver.Add(solver.Sum([x[i, j] for i in range(num_students)]) <= int(schools[j][1]))
+    solver.Add(solver.Sum([y4[i, j] for i in range(num_students)]) <= int(schools[j][1]))
+    solver.Add(solver.Sum([y5[i, j] for i in range(num_students)]) <= int(schools[j][1]))
+
+for j in range(num_pathways):
+    solver.Add(solver.Sum([path[i, j] for i in range(num_students)]) <= int(pathways[j][1]))
 
 objective_terms = []
 for i in range(num_students):
     for j in range(num_schools):
-        objective_terms.append(ranks[i][j] * x[i, j])
+        objective_terms.append(y4_ranks[i][j] * y4[i, j])
+        objective_terms.append(y5_ranks[i][j] * y5[i, j])
+    
+    for j in range(num_pathways):
+        objective_terms.append(path_ranks[i][j] * path[i, j])
 solver.Minimize(solver.Sum(objective_terms))
 
 status = solver.Solve()
 
-print(f"Sum of Rankings = {int(solver.Objective().Value())}\n")
 for i in range(num_students):
     for j in range(num_schools):
-        # Test if x[i,j] is 1 (with tolerance for floating point arithmetic)
-        if x[i, j].solution_value() > 0.5:
-            print(f"{students[i][0]} assigned to {schools[j][0]} which they ranked {ranks[i][j]}")
+        if y4[i, j].solution_value() > 0.5:
+            print(f"{students[i][0]} assigned to {schools[j][0]} in Year 4")
+
+    for j in range(num_schools):
+        if y5[i, j].solution_value() > 0.5:
+            print(f"{students[i][0]} assigned to {schools[j][0]} in Year 5")
+
+    for j in range(num_pathways):
+        if path[i, j].solution_value() > 0.5:
+            print(f"{students[i][0]} assigned to the pathway {pathways[j][0]}\n")
