7adb50053eeb97d7d9ccfba716df63c9a4158204
Ronald Chiang <z5419773@ad.unsw.edu.au>
2023-08-19 08:49:24 +1000

    improving style

--- placements.py
+++ placements.py
@@ -12,8 +12,11 @@ from ortools.linear_solver import pywraplp
 # Scaling for preference between Year 4, Year 5 and Pathways
 scaling = [1, 1.2, 1.5]
 
-def load_data(file_name, delimiter=','):
-    return list(csv.reader(open(file_name, 'r'), delimiter=','))
+def load_data(file_name, delim=','):
+    return list(csv.reader(open(file_name, 'r'), delimiter=delim))
+
+def load_all(*files):
+    return [load_data(file) for file in files]
 
 def check_errors(schools, pathways, students):
     if len(students) > sum([int(capacity[1]) for capacity in schools]):
@@ -29,90 +32,95 @@ def check_errors(schools, pathways, students):
 def calc_ranks(students, start, end, scale):
     return [[int(rank) * scaling[int(student[scale]) - 1] for rank in student[start:end]] for student in students]
 
-if __name__ == "__main__":
+def solver_variables(solver, num_students, num_choices):
+    variables = {}
+    for i in range(num_students):
+        for j in range(num_choices):
+            variables[i, j] = solver.IntVar(0, 1, "")
+    
+    return variables
+
+def set_constraints(solver, num_students, schools, pathways, y4, y5, path):
+    # Each student is assigned to exactly 1 school in y4, y5 and 1 pathway
+    for i in range(num_students):
+        solver.Add(solver.Sum([y4[i, j] for j in range(len(schools))]) == 1)
+        solver.Add(solver.Sum([y5[i, j] for j in range(len(schools))]) == 1)
+        solver.Add(solver.Sum([path[i, j] for j in range(len(pathways))]) == 1)
+
+        # There must be at least 1 metro school between y4 and y5
+        solver.Add(
+            solver.Sum([y4[i, j] * int(schools[j][2] == 'M') for j in range(len(schools))]) + 
+            solver.Sum([y5[i, j] * int(schools[j][2] == 'M') for j in range(len(schools))]) 
+            >= 1
+        )
+
+    # Each school and pathway is assigned to at most the given capacity
+    for j in range(len(schools)):
+        solver.Add(solver.Sum([y4[i, j] for i in range(num_students)]) <= int(schools[j][1]))
+        solver.Add(solver.Sum([y5[i, j] for i in range(num_students)]) <= int(schools[j][1]))
+
+    for j in range(len(pathways)):
+        solver.Add(solver.Sum([path[i, j] for i in range(num_students)]) <= int(pathways[j][1]))
+
+def add_obj_terms(choices, students, ranks, variables):
+    objective_terms = []
+    for i in range(len(students)):
+        for j in range(len(choices)):
+            objective_terms.append(ranks[i][j] * variables[i, j])
+    
+    return objective_terms
+
+def find_choice(solution, student, choices):
+    for i in range(len(choices)):
+        if solution[student, i].solution_value() > 0.5:
+            return choices[i][0]
+        
+def print_solution(students, y4, y5, path, schools, pathways):
+    for i in range(len(students)):
+        y4_school = find_choice(y4, i, schools)
+        print(f"{students[i][0]} assigned to {y4_school} in Year 4")
+
+        y5_school = find_choice(y5, i, schools)
+        print(f"{students[i][0]} assigned to {y5_school} in Year 5")
+
+        pathway = find_choice(path, i, pathways)
+        print(f"{students[i][0]} assigned to the pathway {pathway}\n")
+
+def main():
     if len(sys.argv) != 4:
         print('Usage: python3 placements.py [school file] [pathway file] [student file]')
         sys.exit(1)
 
-    schools = load_data(sys.argv[1])
-    pathways = load_data(sys.argv[2])
-    students = load_data(sys.argv[3])
-
-    num_students = len(students)
-    num_pathways = len(pathways)
-    num_schools = len(schools)
+    schools, pathways, students = load_all(sys.argv[1], sys.argv[2], sys.argv[3])
 
     check_errors(schools, pathways, students)
 
-    cols = [1, num_schools + 1, 2 * num_schools + 1, 2 * num_schools + num_pathways + 1]
+    cols = [1, len(schools) + 1, 2 * len(schools) + 1, 2 * len(schools) + len(pathways) + 1]
     y4_ranks = calc_ranks(students, cols[0], cols[1], cols[3])
     y5_ranks = calc_ranks(students, cols[1], cols[2], cols[3] + 1)
     path_ranks = calc_ranks(students, cols[2], cols[3], cols[3] + 2)
 
     solver = pywraplp.Solver.CreateSolver("SCIP")
 
-    # having the solver assign either 0 or 1 where 1 means  the option is chosen
-    y4, y5, path = {}, {}, {}
-    for i in range(num_students):
-        for j in range(num_schools):
-            y4[i, j] = solver.IntVar(0, 1, "")
-            y5[i, j] = solver.IntVar(0, 1, "")
-        
-        for j in range(num_pathways):
-            path[i, j] = solver.IntVar(0, 1, "")
+    y4 = solver_variables(solver, len(students), len(schools))
+    y5 = solver_variables(solver, len(students), len(schools))
+    path = solver_variables(solver, len(students), len(pathways))
 
-
-    # Constraints
-
-    # Each student is assigned to exactly 1 school in y4, y5 and 1 pathway
-    for i in range(num_students):
-        solver.Add(solver.Sum([y4[i, j] for j in range(num_schools)]) == 1)
-        solver.Add(solver.Sum([y5[i, j] for j in range(num_schools)]) == 1)
-        solver.Add(solver.Sum([path[i, j] for j in range(num_pathways)]) == 1)
-
-        # There must be at least 1 metro school between y4 and y5
-        solver.Add(
-            solver.Sum([y4[i, j] * int(schools[j][2] == 'M') for j in range(num_schools)]) + 
-            solver.Sum([y5[i, j] * int(schools[j][2] == 'M') for j in range(num_schools)]) 
-            >= 1
-        )
-
-    # Each school and pathway is assigned to at most the given capacity
-    for j in range(num_schools):
-        solver.Add(solver.Sum([y4[i, j] for i in range(num_students)]) <= int(schools[j][1]))
-        solver.Add(solver.Sum([y5[i, j] for i in range(num_students)]) <= int(schools[j][1]))
-
-    for j in range(num_pathways):
-        solver.Add(solver.Sum([path[i, j] for i in range(num_students)]) <= int(pathways[j][1]))
-
+    set_constraints(solver, len(students), schools, pathways, y4, y5, path)
 
     # adding how the cost is calculated to the solver
-    objective_terms = []
-    for i in range(num_students):
-        for j in range(num_schools):
-            objective_terms.append(y4_ranks[i][j] * y4[i, j])
-            objective_terms.append(y5_ranks[i][j] * y5[i, j])
-        
-        for j in range(num_pathways):
-            objective_terms.append(path_ranks[i][j] * path[i, j])
+    objective_terms = add_obj_terms(schools, students, y4_ranks, y4) + \
+                      add_obj_terms(schools, students, y5_ranks, y5) + \
+                      add_obj_terms(pathways, students, path_ranks, path)
+
     solver.Minimize(solver.Sum(objective_terms))
 
     status = solver.Solve()
-
     if status not in [pywraplp.Solver.OPTIMAL, pywraplp.Solver.FEASIBLE]:
         print("No possible assignment found")
         sys.exit()
 
-    # printing out the solution
-    for i in range(num_students):
-        for j in range(num_schools):
-            if y4[i, j].solution_value() > 0.5:
-                print(f"{students[i][0]} assigned to {schools[j][0]} in Year 4")
+    print_solution(students, y4, y5, path, schools, pathways)
 
-        for j in range(num_schools):
-            if y5[i, j].solution_value() > 0.5:
-                print(f"{students[i][0]} assigned to {schools[j][0]} in Year 5")
-
-        for j in range(num_pathways):
-            if path[i, j].solution_value() > 0.5:
-                print(f"{students[i][0]} assigned to the pathway {pathways[j][0]}\n")
+if __name__ == "__main__":
+    main()
